2d306b7a0ab2b4e9ffbf83b0bf85ee174a18e7f4
Orin Fletcher <orin.fletcher@gmail.com>
2021-01-01 21:35:50 -0800

    creates about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple`}}>
+      <h1>Hello Orin!</h1>
+      <p>Congrats on not dying in 2020.  Better luck in 2021.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="random-unsplash-image" />
+    </div>
+  )
 }
