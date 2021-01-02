d4b97d7620ca3b5c9f583327c0a760134a6a16c8
Orin Fletcher <orin.fletcher@gmail.com>
2021-01-01 22:26:27 -0800

    basics done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
+    <div style={{ color: `purple` }}>
+      <Link to="/">Home</Link>
+      <Header headerText="About Gatsby" />
+      <Header headerText="Component reusability!" />
       <p>Such wow. Very React.</p>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple`}}>
-      <h1>Hello Orin!</h1>
-      <p>Congrats on not dying in 2020.  Better luck in 2021.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="random-unsplash-image" />
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Orin!" />
+      <p>Congrats on not dying in 2020. Better luck in 2021.</p>
+      <img
+        src="https://source.unsplash.com/random/400x200"
+        alt="random-unsplash"
+      />
     </div>
   )
 }
